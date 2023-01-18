4d60fdfa60c0ddae5c1607181438b14838f76f93
Freddie <jackpot.pudding.06@icloud.com>
2023-01-18 10:10:24 +0100

    Array Übung

---
+++ src/übung7/ArrayUebung.java
@@ -0,0 +1,32 @@
+package übung7;
+
+public class ArrayUebung {
+
+    public static void main(String args[]) {
+
+        // 1. Deklaration eines Arrays für ganze Zahlen.
+        int [] IntArr;
+
+        // 2. Initialisierung des Arrays mit 100 Elementen.
+
+        for (i = 0; i < 100; )
+
+        // 3. Durchlaufen des gesamten Arrays und Ausgabe des Inhalts.
+
+        // 4. Das Array mit allen ganzen Zahlen von 1 bis 100 befüllen.
+
+        // 5. Geben Sie den Wert an der Stelle 89 des Arrays aus.
+
+        // 6. Ändern Sie den Wert des Arrayelements mit dem Index 49 zu 1060.
+
+        // Außerdem ändern Sie den Wert an der ersten und der letzte Stelle des Arrays zu 2020.
+
+        // 7. Erneutes ausgeben des Arrayinhalts.
+
+        // Darstellung: Index und zugehöriger Inhalt (z.B. Index 6 - Inhalt: 7)
+
+        // 8. Berechnung des Durchschnitts aller Arrayelemente
+
+    }
+
+}
